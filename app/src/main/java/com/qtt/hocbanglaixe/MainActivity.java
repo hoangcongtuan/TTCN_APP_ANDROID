c5ccb2bed7c4522a50433102b728f4d707ccbf64
Hoang Cong Tuan <hoangcongtuan@Hoangs-MacBook-Pro.local>
2018-11-22 10:23:09 +0700

    siginin with google, add debug keystore

--- app/src/main/java/com/qtt/hocbanglaixe/MainActivity.java
+++ app/src/main/java/com/qtt/hocbanglaixe/MainActivity.java
@@ -1,13 +1,95 @@
 package com.qtt.hocbanglaixe;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tvUserName)
+    TextView tvUserName;
+    @BindView(R.id.tvEmail)
+    TextView tvEmail;
+    @BindView(R.id.imgAvatar)
+    CircleImageView imgAvatar;
+
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        init();
+        initWidget();
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                loadUserData();
+            }
+        }.run();
+    }
+
+    private void loadUserData() {
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account == null) {
+            //comback to login Activity
+            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
+        }
+
+        tvUserName.setText(account.getDisplayName());
+        tvEmail.setText(account.getEmail());
+//        ImageLoader.getInstance().displayImage(account.getPhotoUrl().toString(), imgAvatar);
+    }
+
+    private void init() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestProfile()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void initWidget() {
+
+    }
+
+    @OnClick(R.id.btnLogout)
+    public void onBtnLogoutClick(View view) {
+        mGoogleSignInClient.signOut().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(MainActivity.this, R.string.logout_success, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, R.string.logout_failed, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
